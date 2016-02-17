f7ac7bd3d43197ea7223513e3c2a4d26b5cbfd19
Alexander Haase <alexander.haase@rwth-aachen.de>
2016-02-17 22:48:06 +0100

    Added first files for concierge.

---
+++ src/concierge/concierge.c
@@ -0,0 +1,33 @@
+/* This file is part of rbl-tools.
+ *
+ * rbl-tools is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Copyright (C)
+ *  2015-2016 Alexander Haase <alexander.haase@rwth-aachen.de>
+ */
+
+#include "concierge.h"
+
+#include <stdlib.h> // EXIT_ macros
+
+#include "config.h"
+
+int
+main(int argc, char **argv)
+{
+	concierge_parse_config(argc, argv);
+
+	return EXIT_SUCCESS;
+}
---
+++ src/concierge/config.c
@@ -0,0 +1,40 @@
+/* This file is part of rbl-tools.
+ *
+ * rbl-tools is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Copyright (C)
+ *  2015-2016 Alexander Haase <alexander.haase@rwth-aachen.de>
+ */
+
+#include "config.h"
+
+#include <argp.h>
+
+
+// Informative data for ARGP.
+const char *argp_program_bug_address =
+    "https://github.com/alehaa/rbl-tools/issues";
+
+static char doc[] = "concierge -- realtime blacklist nfqueues handler";
+
+
+static struct argp argp = {NULL, NULL, NULL, doc};
+
+
+void
+concierge_parse_config(int argc, char **argv)
+{
+	argp_parse(&argp, argc, argv, 0, 0, 0);
+}
